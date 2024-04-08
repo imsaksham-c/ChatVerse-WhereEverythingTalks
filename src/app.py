3bdec78859de2eb5010fe2bef8d88a87e221e786
Saksham Chaurasia <imsaksham.c@gmail.com>
2024-04-08 18:20:32 +0530

    updated changes

--- src/app.py
+++ src/app.py
@@ -1,3 +1,5 @@
+import os
+import shutil
 import streamlit as st
 from utils.get_urls import scrape_urls
 from langchain_core.messages import AIMessage, HumanMessage
@@ -10,92 +12,144 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 
-
+# Load environment variables
 load_dotenv()
 
 def get_vectorstore_from_url(url, max_depth):
+    """
+    Scrapes website content and creates a vector store based on the provided URL and scraping depth.
+
+    Args:
+        url (str): The URL of the website to scrape.
+        max_depth (int): The maximum depth for scraping.
+
+    Returns:
+        tuple: A tuple containing the created vector store and the number of URLs scraped.
+    """
+    if os.path.exists('.chroma'):
+        shutil.rmtree('.chroma')
+
+    # Scrape URLs
     urls = scrape_urls(url, max_depth)
-    # get the text in document form
+
+    # Load website content
     loader = WebBaseLoader(urls)
     document = loader.load()
-    
-    # split the document into chunks
+
+    # Split document into chunks
     text_splitter = RecursiveCharacterTextSplitter()
     document_chunks = text_splitter.split_documents(document)
-    
-    # create a vectorstore from the chunks
+
+    # Create vector store
     vector_store = Chroma.from_documents(document_chunks, OpenAIEmbeddings())
 
     return vector_store, len(urls)
 
 def get_context_retriever_chain(vector_store):
+    """
+    Creates a context-aware retriever chain based on the provided vector store.
+
+    Args:
+        vector_store: The vector store to use for retrieval.
+
+    Returns:
+        obj: The created context-aware retriever chain.
+    """
     llm = ChatOpenAI()
-    
     retriever = vector_store.as_retriever()
-    
+
+    # Define prompt
     prompt = ChatPromptTemplate.from_messages([
-      MessagesPlaceholder(variable_name="chat_history"),
-      ("user", "{input}"),
-      ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
     ])
-    
+
+    # Create retriever chain
     retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-    
+
     return retriever_chain
-    
-def get_conversational_rag_chain(retriever_chain): 
-    
+
+def get_conversational_rag_chain(retriever_chain):
+    """
+    Creates a conversational RAG chain based on the provided retriever chain.
+
+    Args:
+        retriever_chain: The retriever chain to use for conversation.
+
+    Returns:
+        obj: The created conversational RAG chain.
+    """
     llm = ChatOpenAI()
-    
+
+    # Define prompt
     prompt = ChatPromptTemplate.from_messages([
-      ("system", "Answer the user's questions based on the below context:\n\n{context}"),
-      MessagesPlaceholder(variable_name="chat_history"),
-      ("user", "{input}"),
+        ("system", "Answer the user's questions based on the below context:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
     ])
-    
+
+    # Create RAG chain
     stuff_documents_chain = create_stuff_documents_chain(llm,prompt)
-    
+
     return create_retrieval_chain(retriever_chain, stuff_documents_chain)
 
 def get_response(user_input):
+    """
+    Gets a response from the chatbot based on user input.
+
+    Args:
+        user_input (str): The user's input message.
+
+    Returns:
+        str: The chatbot's response.
+    """
     retriever_chain = get_context_retriever_chain(st.session_state.vector_store)
     conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
-    
+
     response = conversation_rag_chain.invoke({
         "chat_history": st.session_state.chat_history,
         "input": user_input
     })
-    
+
     return response['answer']
 
-# app config
+# App config
 st.set_page_config(page_title="WebChat 🤖: Chat with Websites", page_icon="🤖")
 st.title("WebChat 🤖: Chat with Websites")
 
+# Initialize session state variables
 if "freeze" not in st.session_state:
     st.session_state.freeze = False
 if "max_depth" not in st.session_state:
     st.session_state.max_depth = 1
 
-# sidebar
+# Sidebar
 with st.sidebar:
-    st.header("Settings")
+    st.header("WebChat 🤖")
     website_url = st.text_input("Website URL")
     
     if not st.session_state.freeze:
         st.session_state.max_depth = st.slider("Select maximum scraping depth:", 1, 5, 1)
+
         if st.button("Proceed"):
             st.session_state.freeze = True
     else:
         st.session_state.max_depth = st.slider("Select maximum scraping depth:", 1, 5, st.session_state.max_depth)
         st.button("Proceed", disabled=True)
+
+    st.sidebar.markdown('---')
+    st.sidebar.markdown('Connect with me:')
+    st.sidebar.markdown('[LinkedIn](https://www.linkedin.com/in/saksham-chaurasia/)')
+    st.sidebar.markdown('[GitHub](https://github.com/imsaksham-c)')
+    st.sidebar.markdown('[Email](mailto:imsaksham.c@gmail.com)')
     
+# Main content
 if website_url is None or website_url == "":
     st.info("Please enter a website URL")
-
 else:
     if st.session_state.freeze:
-        # session state
+        # Initialize chat history and vector store if not already present
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = [
                 AIMessage(content="Hello, I am a bot. How can I help you?"),
@@ -103,26 +157,22 @@ else:
         if "vector_store" not in st.session_state:
             with st.sidebar:
                 with st.spinner("Scrapping Website..."):
-                    st.session_state.vector_store, st.session_state.len_urls = get_vectorstore_from_url(website_url,
-                                                                                                        st.session_state.max_depth)
+                    st.session_state.vector_store, st.session_state.len_urls = get_vectorstore_from_url(website_url, st.session_state.max_depth)
                     st.write(f"Total Pages Scrapped: {st.session_state.len_urls}")
                     st.success("Scraping completed, 🤖 Ready!")
-
         else:
             with st.sidebar:
-                    st.write(f"Total Pages Scrapped: {st.session_state.len_urls}")
-                    st.success("🤖 Ready!")
+                st.write(f"Total Pages Scrapped: {st.session_state.len_urls}")
+                st.success("🤖 Ready!")
 
-        # user input
-        user_query = st.chat_input("Type your message here...")
+        # User input
+        user_query = st.text_input("Type your message here...")
         if user_query is not None and user_query != "":
             response = get_response(user_query)
             st.session_state.chat_history.append(HumanMessage(content=user_query))
             st.session_state.chat_history.append(AIMessage(content=response))
-            
-        
 
-        # conversation
+        # Display conversation
         for message in st.session_state.chat_history:
             if isinstance(message, AIMessage):
                 with st.chat_message("AI"):
